1a0e5bc7b46ef72aa45e0dbacb49ce3a641f413d
tranncl <tranncl@gmail.com>
2024-02-29 16:33:10 +0100

    Implement checkbox management

--- web.py
+++ web.py
@@ -16,8 +16,17 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This is for writing a text")
 
-for todo in todos:
-    st.checkbox(todo)
+# Display the todo list as checkbox & get the indexes
+for index, todo in enumerate(todos):
+    # get the key of each item as the item itself
+    checkbox = st.checkbox(todo, key=todo)
+    # If an item is checked (True): make it disappear, update the file,
+    # update the session state & finally rerun the session.
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label=" ", placeholder="Enter a new todo ... ",
               on_change=add_todo, key="new_todo")
